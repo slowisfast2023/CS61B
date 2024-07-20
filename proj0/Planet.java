77f8c188ca885dd82e9dc85f115c5fc5d37d9843
slowisfast2023 <cdai2012@gmail.com>
2024-07-20 13:18:15 -0400

    added Planet.java

---
+++ proj0/Planet.java
@@ -0,0 +1,38 @@
+public class Planet {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+    public static final double G = 6.67e-11;
+
+    public Planet(double xP, double yP, double xV, double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    public Planet(Planet p) {
+        this.xxPos = p.xxPos;
+        this.yyPos = p.yyPos;
+        this.xxVel = p.xxVel;
+        this.yyVel = p.yyVel;
+        this.mass = p.mass;
+        this.imgFileName = p.imgFileName;
+    }
+
+    public double calcDistance(Planet p){
+        double distance = Math.sqrt(Math.pow((p.xxPos - this.xxPos), 2) + Math.pow((p.yyPos - this.yyPos), 2));
+        return distance;
+    }
+
+    public double calcForceExertedBy(Planet p){
+        double force = Planet.G * this.mass * p.mass / Math.pow(this.calcDistance(p), 2);
+        return force;
+    }
+
+}
