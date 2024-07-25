16fb981cf508e2fb694614140d6e3a6dbe1f8d05
slowisfast2023 <cdai2012@gmail.com>
2024-07-25 18:10:37 -0400

    submit IntList.java for lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList p = A;
+        while (A.rest != null) {
+            A = A.rest;
+        }
+        A.rest = B;
+        return p;
     }
 
     /**
@@ -91,7 +96,11 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return B;
+        } else {
+            return new IntList(A.first, catenate(A.rest, B));
+        }
     }
 
 
